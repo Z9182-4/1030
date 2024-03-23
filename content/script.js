995d467018fc54306315fda752f9928513d93bc6
Zize Yu <yuzize@Zizes-iMac.local>
2024-03-23 00:21:09 -0700

    add backToTop button, modify position of nav,footer li

--- content/script.js
+++ content/script.js
@@ -9,3 +9,19 @@ document.addEventListener('DOMContentLoaded', function() {
         navUl.classList.toggle('show');
     });
 });
+//backToTop button
+document.getElementById("topBtn").addEventListener("click", topFunction);
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
