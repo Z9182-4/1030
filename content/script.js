eca49f60161c6b4cae71f5afe69fcf9d9a27d59c
Z9182-4 <159979188+Z9182-4@users.noreply.github.com>
2024-03-28 13:56:53 -0700

    remove php, modify layout

--- content/script.js
+++ content/script.js
@@ -26,34 +26,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-var forms = document.querySelectorAll('form');
-forms.forEach(function(form) {
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // create a URLSearchParams object from the form data
-    var formData = new URLSearchParams(new FormData(event.target));
-
-    // send a fetch request to the PHP file
-    fetch('submit.php?' + formData.toString())
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text(); // Parse the response as text
-      })
-      .then(text => {
-        console.log('Received text response:', text); // Log the text response
-        // No need to parse the response as JSON since it's already handled in PHP
-        // Process the response as needed
-      })
-      .then(result => {
-        // Show the result for each section
-        alert(result);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
-});
